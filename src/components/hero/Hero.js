d8ec66df49c6dba290104941a04069fc2575e00b
JK1320 <kaurjus1@gmail.com>
2023-01-08 23:25:57 +0000

    added alt attribute to img tag

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -4,7 +4,7 @@ import "./Hero.css";
 const Hero = ({img, text}) => {
   return (
     <div className='hero'>
-        <img src={img} />
+        <img src={img} alt="" />
         <h1>{text}</h1>
     </div>
   )
